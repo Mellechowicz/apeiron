2b177164debbe4a6c469ead9938f32fa62882af6
Andrzej P. Kądzielawa <apkadzielawa@gmail.com>
2021-05-25 18:19:27 +0200

    fd

--- crude.py
+++ crude.py
@@ -5,10 +5,20 @@ from JorGpi.POSCARloader import POSCARloader
 from JorGpi.generate.loadsave import SavePOSCAR
 from sys import argv
 
+# SETTINGS TODO: Read them from cmd line
+
+mean     = 0.0
+deviance = 0.000001 # 1% change
+
+
+# Parsing POSCAR file (first arg)
 parser = POSCARloader(argv[1])
 parser.parse()
 data = parser(0)
 del parser
 
-data['cell'][0] = ('W',np.array([-1.,-1.,-1.]))
+print(data)
+for i,atom in enumerate(data['cell']):
+	data['cell'][i] = (atom[0],np.dot(data['directions'],np.random.normal(mean,deviance,3))+atom[1])
+
 SavePOSCAR(data,fileName='POSCAR.out',multiplyers=[0,0,0])
